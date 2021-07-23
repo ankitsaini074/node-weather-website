f8e9aee11699da287c383335e2554e4a36b245b2
Ankit <ankit@DESKTOP-S28I9ID>
2021-07-23 15:03:48 +0530

    Add wind speed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const url = 'http://api.weatherstack.com/current?access_key=d18bc11ff91de2215ba7
            callback('Unable to find location', undefined)
       }
       else {
-           callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degrees out. It feels like ' + body.current.precip + '% chance of rain.')   
+           callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degrees out. It feels like ' + body.current.precip + '% chance of rain. The wind speed is ' + body.current.wind_speed + '%')   
        }
     })
  }
