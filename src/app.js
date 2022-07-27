2f754a8f767a82d80793ece5f69960f5467201ad
ankitsaini074 <77288569+ankitsaini074@users.noreply.github.com>
2022-07-27 12:45:48 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geoCode = require('./utils/geoCode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = process.env.PORT || 3000
+
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -94,6 +94,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+app.listen(3000, () => {
+    console.log('Server is up on port 3000.')
 })
